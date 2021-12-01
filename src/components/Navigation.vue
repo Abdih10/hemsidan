a24d18f8d8f9ea5f25fb58474392a324a973fc8e
Abdi <abdi_665@hotmail.com>
2021-12-01 13:26:29 +0100

    Added navigation and basic class for contact and utryckningsrapporter

---
+++ src/components/Navigation.vue
@@ -0,0 +1,112 @@
+<template>
+  <!-- navbar goes here -->
+  <nav class="bg-gray-200">
+    <div class="max-w-8xl mx-auto px-4">
+      <!-- Kolla med gruppen om hur långt ifrån varandra länkerna ska vara. Antigen between eller around som det är just nu. -->
+      <div class="flex justify-around">
+        <div class="flex space-x-4">
+          <!-- logo -->
+          <div>
+            <a
+              href="#"
+              class="flex items- center py-5 px-2 text-gray-700 hover:text-gray-900"
+            >
+              <svg
+                class="h-6 w-6 mr-1 text-blue-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                />
+              </svg>
+              <span class="font-bold">PSS Skolan?</span>
+            </a>
+          </div>
+        </div>
+
+        <!-- nav -->
+        <div class="hidden md:flex items-center space-x-1">
+          <router-link
+            class="block py-2 px-4 text-sm hover:bg-gray-200"
+            :to="{ name: 'Home' }"
+            >Home</router-link
+          >
+          <router-link
+            class="block py-2 px-4 text-sm hover:bg-gray-200"
+            :to="{ name: 'Utryckningsrapporter' }"
+            >Utryckningsrapporter</router-link
+          >
+          <router-link
+            class="block py-2 px-4 text-sm hover:bg-gray-200"
+            :to="{ name: 'Contact' }"
+            >Contact</router-link
+          >
+        </div>
+
+        <!-- mobile button goes here -->
+        <div class="md:hidden flex items-center">
+          <button class="mobile-menu-button" v-on:click="toggleNavbar()">
+            <svg
+              class="w-6 h-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!-- mobile menu -->
+    <div class="mobile-menu hidden md:hidden">
+      <router-link
+        class="block py-2 px-4 text-sm hover:bg-gray-200"
+        :to="{ name: 'Home' }"
+        >Home</router-link
+      >
+      <router-link
+        class="block py-2 px-4 text-sm hover:bg-gray-200"
+        :to="{ name: 'Utryckningsrapporter' }"
+        >Utryckningsrapporter</router-link
+      >
+      <router-link
+        class="block py-2 px-4 text-sm hover:bg-gray-200"
+        :to="{ name: 'Contact' }"
+        >Contact</router-link
+      >
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "Navigation",
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+  methods: {
+    toggleNavbar: function () { 
+    const btn = document.querySelector(".mobile-menu-button");
+      const menu = document.querySelector(".mobile-menu");
+      btn.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+      });
+    },
+  },
+};
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
+import Utryckningsrapporter from "../views/Utryckningsrapporter.vue";
+import Contact from "../views/Contact.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,13 +10,14 @@ const routes: Array<RouteRecordRaw> = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/utryckningsrapporter",
+    name: "Utryckningsrapporter",
+    component: Utryckningsrapporter,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact,
   },
 ];
 
