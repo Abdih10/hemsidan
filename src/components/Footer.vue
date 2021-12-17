cf7054e17cd4911a769cee538a2ef62b4e120c43
Abdi Hussein <72185874+Abdih10@users.noreply.github.com>
2021-12-17 09:34:08 +0100

    Footer (#5)
    * Fixed Footer component
    
    * different footer to choose between
    
    * basic footer
    
    * fixed minor buggs

---
+++ src/components/Footer.vue
@@ -0,0 +1,118 @@
+<template>
+  <footer class="bg-gray-300 pt-8 pb-6 fixed inset-x-0 bottom-0 p-4">
+    <div
+      class="bg-cover bg-center"
+      style="
+        background-image: url('https://image.freepik.com/free-photo/abstract-black-white-bokeh-background_1962-1324.jpg');
+      "
+    >
+      <div style="background-color: rgba(0, 0, 0, 0.6)">
+        <div class="container mx-auto px-6 lg:px-20 py-12">
+          <div class="lg:flex">
+            <div class="w-full lg:w-2/3">
+              <div class="lg:flex">
+                <div class="w-full mb-12 lg:mb-0 lg:w-1/2">
+                  <h2
+                    style="font-family: 'Baloo Tamma 2', cursive"
+                    class="font-bold text-xl text-gray-100 mb-4"
+                  >
+                    PT-Teknik
+                  </h2>
+                  <p class="text-gray-400">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                  <div class="flex mt-6">
+                    <i
+                      style="background-color: #3b5998"
+                      class="flex items-center justify-center h-12 w-12 mr-1 rounded-full fab fill-current text-white text-xl fa-facebook-f"
+                    ></i>
+                    <i
+                      style="background-color: #dd4b39"
+                      class="flex items-center justify-center h-12 w-12 mx-1 rounded-full fas fill-current text-white text-xl fa-envelope"
+                    ></i>
+                    <i
+                      style="background-color: #125688"
+                      class="flex items-center justify-center h-12 w-12 mx-1 rounded-full fab fill-current text-white text-xl fa-instagram"
+                    ></i>
+                    <i
+                      style="background-color: #55acee"
+                      class="flex items-center justify-center h-12 w-12 mx-1 rounded-full fab fill-current text-white text-xl fa-twitter"
+                    ></i>
+                  </div>
+                </div>
+                <div class="w-full lg:w-1/2 lg:flex lg:px-6">
+                  <div class="w-full mb-6 lg:mb-0 lg:w-1/2">
+                    <h2
+                      style="font-family: 'Baloo Tamma 2', cursive"
+                      class="font-bold text-gray-100 mb-4"
+                    >
+                      Useful Links
+                    </h2>
+                    <ul class="text-gray-500 text-sm">
+                      <li class="pt-1 pb-2">Home</li>
+                      <li class="pt-1 pb-2">Utryckningsrapporter</li>
+                      <li class="pt-1 pb-2">Contact</li>
+                    </ul>
+                  </div>
+                  <div class="w-full mb-6 lg:mb-0 lg:w-1/2">
+                    <h2
+                      style="font-family: 'Baloo Tamma 2', cursive"
+                      class="font-bold text-gray-100 mb-4"
+                    >
+                      Our sevices
+                    </h2>
+                    <ul class="text-gray-500 text-sm">
+                      <li class="pt-1 pb-2">Hårdvara</li>
+                      <li class="pt-1 pb-2">Mjukvara</li>
+                      <li class="pt-1 pb-2">Systemkonstruktör</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-full lg:w-1/3">
+              <h2
+                style="font-family: 'Baloo Tamma 2', cursive"
+                class="font-bold text-gray-100 mb-4"
+              >
+                Nyhetsbrev
+              </h2>
+              <div class="text-gray-300 mb-8">
+                Subscribe our weekly Newsletter to get regular updates about our
+                systems.
+              </div>
+              <div class="flex">
+                <input type="text" class="rounded-l px-2 focus:outline-none" />
+                <button
+                  class="text-gray-200 rounded-r hover:bg-blue-500 px-4 py-2 focus:outline-none"
+                  style="background-color: #dc0900"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        style="
+          font-family: 'Baloo Tamma 2', cursive;
+          background-color: rgba(0, 0, 0, 0.7);
+        "
+      >
+        <div class="container mx-auto px-6 lg:px-20 py-6">
+          <div class="flex justify-center text-gray-300 mb-1">
+            © 2021 <span class="font-bold"> PT-Teknik </span> 2021 All right
+            reserved.
+          </div>
+          <div class="flex font-light justify-center text-gray-500 text-sm">
+            <p>Designed by <span class="font-bold">PT-Teknik</span></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,19 @@
   <div class="home">
     <h1>This is the home page!</h1>
   </div>
+  <Footer />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import Navigation from "../components/Navigation.vue";
+import Footer from "../components/Footer.vue";
 
 @Options({
-  components: {},
+  components: {
+    Navigation,
+    Footer,
+  },
 })
 export default class Home extends Vue {}
 </script>
