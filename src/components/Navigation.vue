6d8409edcc6701e2881d0cf840c2ca69d56c5eda
Abdi <abdi_665@hotmail.com>
2021-12-17 09:49:51 +0100

    fixed some minor buggs

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -39,38 +39,14 @@
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
-          <button
-            class="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            type="button"
-            style="transition: all 0.15s ease 0s"
-          >
-            <router-link
-              class="lg:text-blue lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-s font-bold"
-              :to="{ name: 'Login' }"
-              >Login</router-link
-            >
-          </button>
-        </div>
-
-        <!-- mobile button goes here -->
-        <div class="md:hidden flex items-center">
-          <button class="mobile-menu-button" v-on:click="toggleNavbar()">
-            <svg
-              class="w-6 h-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
       </div>
       <div
         class="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none"
@@ -98,17 +74,17 @@
               >Contact</router-link
             >
           </li>
-           <button
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-        style="transition: all 0.15s ease 0s"
-      >
-        <router-link
-          class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-s font-bold"
-          :to="{ name: 'Login' }"
-          >Login</router-link
-        >
-      </button>
+          <button
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="button"
+            style="transition: all 0.15s ease 0s"
+          >
+            <router-link
+              class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-s font-bold"
+              :to="{ name: 'Login' }"
+              >Login</router-link
+            >
+          </button>
         </ul>
       </div>
     </div>
