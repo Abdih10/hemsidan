711fec4cb8cb24b9f66a9f0a5eab69e5e2f8aa7a
Abdi <abdi_665@hotmail.com>
2021-12-17 01:28:42 +0100

    basic calendar for the website

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,25 @@
   <div class="home">
     <h1>This is the home page!</h1>
   </div>
+  <vue-cal
+    :time="false"
+    hide-weekends
+    class="vuecal--rounded-theme vuecal--blue-theme"
+    style="width: 600px; height: 400px"
+  ></vue-cal>
+  />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import Navigation from "../components/Navigation.vue";
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
 
 @Options({
   components: {
     Navigation,
+    VueCal,
   },
 })
 export default class Home extends Vue {}
