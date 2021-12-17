407c5b3ef606e1b67df399bb009bd6144ca7e637
Abdi <abdi_665@hotmail.com>
2021-12-17 11:28:58 +0100

    Moved vue calendar to a simple component instead having it in the home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,20 @@
 <template>
   <div class="home">
-    <h1>This is the home page!</h1>
+    <VueCalendar />
   </div>
-  <vue-cal
-    :time="false"
-    hide-weekends
-    class="vuecal--rounded-theme vuecal--blue-theme"
-    style="width: 600px; height: 400px"
-  ></vue-cal>
-  />
   <Sections />
   <Footer />
-
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import Navigation from "../components/Navigation.vue";
-import VueCal from "vue-cal";
-import "vue-cal/dist/vuecal.css";
+import VueCalendar from "../components/VueCalendar.vue";
 import Sections from "../components/Sections.vue";
 import Footer from "../components/Footer.vue";
 
-
 @Options({
   components: {
-    Navigation,
-    VueCal,
+    VueCalendar,
     Sections,
     Footer,
   },
