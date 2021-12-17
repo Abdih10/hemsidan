c03b2fba73b39a4c5d73c3c625889e4e0f9da9d8
Abdi <abdi_665@hotmail.com>
2021-12-17 01:12:23 +0100

    basic cards for the different topics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,18 @@
   <div class="home">
     <h1>This is the home page!</h1>
   </div>
+  <Sections />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import Navigation from "../components/Navigation.vue";
+import Sections from "../components/Sections.vue";
 
 @Options({
   components: {
     Navigation,
+    Sections,
   },
 })
 export default class Home extends Vue {}
