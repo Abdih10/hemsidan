55b10501badc5f55c5167e1490ce107e52ece2d8
Abdi Hussein <72185874+Abdih10@users.noreply.github.com>
2021-12-17 09:52:53 +0100

    Contact (#7)
    * basic contact form
    
    * basic cards for the different topics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,22 @@
   <div class="home">
     <h1>This is the home page!</h1>
   </div>
+  <Sections />
   <Footer />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import Navigation from "../components/Navigation.vue";
+import Sections from "../components/Sections.vue";
 import Footer from "../components/Footer.vue";
 
 @Options({
   components: {
     Navigation,
+    Sections,
     Footer,
+
   },
 })
 export default class Home extends Vue {}
