cb6371a0c1f31f28feca81a92fac06e9a9d42f33
Abdi <abdi_665@hotmail.com>
2021-12-20 11:29:46 +0100

    Fixed minor buggs and added rapport form to the project

--- src/views/Utryckningsrapporter.vue
+++ src/views/Utryckningsrapporter.vue
@@ -1,12 +1,15 @@
 <template>
   <ContactForm />
+  <Rapport />
 </template>
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import Rapport from "@/components/Rapport.vue";
 import ContactForm from "../components/ContactForm.vue";
 @Options({
   components: {
     ContactForm,
+    Rapport,
   },
 })
 export default class Utryckningsrapporter extends Vue {}
